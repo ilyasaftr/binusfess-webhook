40d30d11ae64ac96acd8586a1c83d89bdf02b78d
ilyasa0x <ilyasarecord@gmail.com>
2023-01-31 10:20:01 +0700

    add console log in development mode

--- app.js
+++ app.js
@@ -7,14 +7,26 @@ async function main() {
   sentryInit(process.env.SENTRY_DSN);
 
   twict.onDirectMessage((event) => {
+    if (process.env.NODE_ENV != "production") {
+      console.log('[Triggered] event_directMessage')
+    }
+
     rabbitSendMessage('event_directMessage', event);
   });
 
   twict.onFollow((event) => {
+    if (process.env.NODE_ENV != "production") {
+      console.log('[Triggered] event_follow')
+    }
+
     rabbitSendMessage('event_follow', event);
   });
 
   twict.onTweetCreate((event) => {
+    if (process.env.NODE_ENV != "production") {
+      console.log('[Triggered] event_tweetCreate')
+    }
+
     rabbitSendMessage('event_tweetCreate', event);
   });
 
